0e01a3417a159bd7167c9583eec5b4580bed526f
Anupama Dissanayake <61502343+Anu2006-dev@users.noreply.github.com>
2020-08-13 18:37:00 +0530

    main.js loads react-server output
    main.js loads localhost:3000 (or any other port which react runs) into the electron app

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(`https://localhost:${process.env.PORT ? process.env.PORT : 3000}`)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
